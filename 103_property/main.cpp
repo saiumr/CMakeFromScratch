6d7ab76773c9d75791df351765d4252afcabe0bd
saiumr <colaloveanan@gmail.com>
2026-02-05 10:40:58 +0800

    目标属性设置宏变量

--- 103_property/main.cpp
+++ 103_property/main.cpp
@@ -2,6 +2,17 @@
 
 int main(int argc, char* argv[]) {
     std::cout << "Test cmake property" << std::endl;
-    std::cout << PARA1 << std::endl;  // cmake 文件属性传递的宏
+    #ifdef PARA1
+    std::cout << "PARA1 = " << PARA1 << std::endl;  // cmake 文件属性传递的宏
+    #endif
+
+    #ifdef PARA2
+    std::cout << "PARA2 = " << PARA2 << std::endl;  // cmake 目标属性传递的宏
+    #endif
+    
+    #ifdef PARA3
+    std::cout << "PARA3 = " << PARA3 << std::endl;  // cmake 目标属性传递的宏
+    #endif
+    
     return 0;
 }
